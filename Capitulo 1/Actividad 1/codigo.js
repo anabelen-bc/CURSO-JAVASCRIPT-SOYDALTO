5d3438e240bd9652293ab5940f0a9f9adb49bd12
anabelen-bc <anabelenn.bc@gmail.com>
2023-03-21 17:34:13 -0300

    Actividad 1

--- Capitulo 1/Actividad 1/codigo.js
+++ Capitulo 1/Actividad 1/codigo.js
@@ -1,30 +1,37 @@
-let dineroCofla = prompt("Cuanto dinero tienes?");
+let dineroCofla = prompt("Cuanto dinero tienes Cofla?");
+dineroCofla=parseInt(dineroCofla);
 
 if (dineroCofla >= 0.6 && dineroCofla < 1) {
     alert("Cofla, comprate el helado de agua");
+    alert("y te sobran " + (dineroCofla - 0.6));
 }
 else if (dineroCofla >= 1 && dineroCofla < 1.6) {
     alert("Cofla, comprate el helado de crema");
+    alert("y te sobran " + (dineroCofla - 1));
 }
 else if (dineroCofla >= 1.6 && dineroCofla < 1.7) {
     alert("Cofla, comprate el helado de heladix");
+    alert("y te sobran " + (dineroCofla - 1.6));
 }
 else if (dineroCofla >= 1.7 && dineroCofla < 1.8) {
     alert("Cofla, comprate el helado de heladovich");
+    alert("y te sobran " + (dineroCofla - 1.7));
 }
 else if (dineroCofla >= 1.8 && dineroCofla < 2.9) {
     alert("Cofla, comprate el helado de helardo");
+    alert("y te sobran " + (dineroCofla - 1.8));
 }
 
 else if (dineroCofla >= 2.9) {
     alert("Cofla, comprate el helado con confites o el pote de 1/4 kg");
+    alert("y te sobran " + (dineroCofla - 2.9));
 }
 
 else {
     alert("Cofla, no te alcanza para ningun helado");
 }
 
-let dineroRoberto = prompt("Cuanto dinero tienes?");
+let dineroRoberto = prompt("Cuanto dinero tienes Roberto?");
 
 if (dineroRoberto >= 0.6 && dineroRoberto < 1) {
     alert("Roberto, comprate el helado de agua");
@@ -50,7 +57,7 @@ else {
     alert("Roberto, no te alcanza para ningun helado");
 }
 
-let dineroPedro = prompt("Cuanto dinero tienes?");
+let dineroPedro = prompt("Cuanto dinero tienes Pedro?");
 
 if (dineroPedro >= 0.6 && dineroPedro < 1) {
     alert("Pedro, comprate el helado de agua");
