09bb2de43c68ff86c94457c166e4e0def7698a19
anabelen-bc <anabelenn.bc@gmail.com>
2023-03-21 10:39:24 -0300

    Concatenacion, Strings y Backticks

---
+++ Nivel 1/Concatenacion/codigo.js
@@ -0,0 +1,16 @@
+saludo = "Hola Pedro";
+pregunta = "¿Como estas?";
+
+frase = saludo +" " + pregunta;
+document.write(frase);
+
+numero1 = 5;
+numero2 = 8;
+
+suma = `numero1.concat(numero2)`;
+
+document.write(suma);
+
+nombre = "Ani"
+frasefinal= `Hola ${nombre} como estas?`;
+document.write(frasefinal)
