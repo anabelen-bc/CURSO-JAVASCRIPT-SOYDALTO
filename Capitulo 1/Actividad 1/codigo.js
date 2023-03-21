fe9ae3849c95a3c91d06e4e4d36175527ae25427
anabelen-bc <anabelenn.bc@gmail.com>
2023-03-21 17:14:20 -0300

    Actividad 1

---
+++ Capitulo 1/Actividad 1/codigo.js
@@ -0,0 +1,77 @@
+let dineroCofla = prompt("Cuanto dinero tienes?");
+
+if (dineroCofla >= 0.6 && dineroCofla < 1) {
+    alert("Cofla, comprate el helado de agua");
+}
+else if (dineroCofla >= 1 && dineroCofla < 1.6) {
+    alert("Cofla, comprate el helado de crema");
+}
+else if (dineroCofla >= 1.6 && dineroCofla < 1.7) {
+    alert("Cofla, comprate el helado de heladix");
+}
+else if (dineroCofla >= 1.7 && dineroCofla < 1.8) {
+    alert("Cofla, comprate el helado de heladovich");
+}
+else if (dineroCofla >= 1.8 && dineroCofla < 2.9) {
+    alert("Cofla, comprate el helado de helardo");
+}
+
+else if (dineroCofla >= 2.9) {
+    alert("Cofla, comprate el helado con confites o el pote de 1/4 kg");
+}
+
+else {
+    alert("Cofla, no te alcanza para ningun helado");
+}
+
+let dineroRoberto = prompt("Cuanto dinero tienes?");
+
+if (dineroRoberto >= 0.6 && dineroRoberto < 1) {
+    alert("Roberto, comprate el helado de agua");
+}
+else if (dineroRoberto >= 1 && dineroRoberto < 1.6) {
+    alert("Roberto, comprate el helado de crema");
+}
+else if (dineroRoberto >= 1.6 && dineroRoberto < 1.7) {
+    alert("Roberto, comprate el helado de heladix");
+}
+else if (dineroRoberto >= 1.7 && dineroRoberto < 1.8) {
+    alert("Roberto, comprate el helado de heladovich");
+}
+else if (dineroRoberto >= 1.8 && dineroRoberto < 2.9) {
+    alert("Roberto, comprate el helado de helardo");
+}
+
+else if (dineroRoberto >= 2.9) {
+    alert("Roberto, comprate el helado con confites o el pote de 1/4 kg");
+}
+
+else {
+    alert("Roberto, no te alcanza para ningun helado");
+}
+
+let dineroPedro = prompt("Cuanto dinero tienes?");
+
+if (dineroPedro >= 0.6 && dineroPedro < 1) {
+    alert("Pedro, comprate el helado de agua");
+}
+else if (dineroPedro >= 1 && dineroPedro < 1.6) {
+    alert("Pedro, comprate el helado de crema");
+}
+else if (dineroPedro>= 1.6 && dineroPedro < 1.7) {
+    alert("Pedro, comprate el helado de heladix");
+}
+else if (dineroPedro >= 1.7 && dineroPedro < 1.8) {
+    alert("Pedro, comprate el helado de heladovich");
+}
+else if (dineroPedro >= 1.8 && dineroPedro < 2.9) {
+    alert("Pedro, comprate el helado de helardo");
+}
+
+else if (dineroPedro >= 2.9) {
+    alert("Pedro, comprate el helado con confites o el pote de 1/4 kg");
+}
+
+else {
+    alert("Pedro, no te alcanza para ningun helado");
+}
