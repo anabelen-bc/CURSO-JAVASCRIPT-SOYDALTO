18a02f3cdb38043ef4b335601a4c90b2e9528cd1
anabelen-bc <anabelenn.bc@gmail.com>
2023-03-21 10:09:55 -0300

    Operadores

---
+++ Nivel 1/Operadores/codigo.js
@@ -0,0 +1,44 @@
+/*Suma*/
+
+let numero = 10;
+numero += 5;
+
+document.write(numero)
+ 
+
+/*Resta*/
+
+let numero2 = 10;
+numero2 -= 5;
+
+document.write(numero2)
+
+/*Multiplicacion */
+
+let numero3 = 10;
+numero3 += 5;
+
+document.write(numero3)
+
+/*Division*/
+
+let numero4 = 10;
+numero4 /= 5;
+
+document.write(numero4)
+
+/*Resto*/
+
+let numero5 = 10;
+numero5 %= 5;
+
+document.write(numero5)
+
+/*Exponencial ELEVADO*/
+
+let numero6 = 10;
+numero6 **= 5;
+
+document.write(numero6)
+
+
