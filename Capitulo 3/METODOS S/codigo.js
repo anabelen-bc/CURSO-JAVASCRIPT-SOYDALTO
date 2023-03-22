3d2a025fd552459626ebb82941e4f81af58eff1e
anabelen-bc <anabelenn.bc@gmail.com>
2023-03-22 13:27:14 -0300

    Static - Set - Get

--- Capitulo 3/METODOS S/codigo.js
+++ Capitulo 3/METODOS S/codigo.js
@@ -20,31 +20,28 @@ verInfo(){
 }
 
 
-class perro extends animal {
+class Perro extends animal {
     constructor(especie,edad,color, raza){
         super(especie,edad,color);
-        this.raza = raza;
+        this.raza = null;
     }
     
-    static ladrar(){
-        alert("¡Wawaw!")
-    } //  Un metodo statico funciona por si solo
+    // static ladrar(){
+    //     alert("¡Wawaw!")
+    // } //  Un metodo statico funciona por si solo
+   set setRaza(newName){
+    this.raza =newName;
+   }
 
-    set setRaza(newName){
-        this.raza = newName;
-    }
-
-    get getRaza(){
-        return this.raza;
-    }
+   get getRaza (){
+    return this.raza;
+   }
 }
 
 // No se puede tener un objeto con el mismo nombre que una clase 
-
-const perroo = new animal ("perro", 5, "marron", "doberman");
+const perro = new Perro ("perro", 5, "marron", "doberman");
 const gato = new animal ("gato", 7, "blanco");
 const pajaro = new animal ("pajaro", 1, "verde");
 
-perro.setRaza = "Pedro"
-
+perro.setRaza = "Pedro";
 document.write(perro.getRaza);
