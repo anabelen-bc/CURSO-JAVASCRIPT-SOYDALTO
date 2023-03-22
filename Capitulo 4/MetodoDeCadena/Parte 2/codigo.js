3cf8d1c27149d23acc6ef48bbd5665931a3dbe45
anabelen-bc <anabelenn.bc@gmail.com>
2023-03-22 20:49:45 -0300

    Metodo de Cadena Parte 2

--- Capitulo 4/MetodoDeCadena/Parte 2/codigo.js
+++ Capitulo 4/MetodoDeCadena/Parte 2/codigo.js
@@ -1,21 +1,18 @@
 let palabras = "Hola como estas?";
 let palabra = "como";
 
-// resultado2 = palabras.includes(palabra);
-
-// document.write(resultado2);
- 
-
-//IndexOf nos devuelve la posicion donde comienza la primer letra de la palabra que le indicamos.
-// LastIndexOf, nos devuelve la ultima posicion de la letra de la palabra que conincide 
-resultado3 = palabras.indexOf(palabra);
-
-document.write(resultado3);
-
-resultado4 = palabras.lastIndexOf(palabra);
-document.write(resultado4);
-
 //padStart nos rellena el princio de nuestra cadena de caracteres, hasta las posiciones que se indica, con la letra que se le indica
 
-resultado5 = palabra.padStart(10, "a");
-document.write(resultado5);
+// resultado5 = palabra.padStart(10, "a");
+// document.write(resultado5);
+
+//padEnd nos rellena el final de nuestra cadena de caracteres 
+ 
+// resultado6 = palabra.padEnd(10, "A");
+// document.write(resultado6);
+
+
+//la opcion de Repeat() hace que se repita tantas veces como lo indiquemos
+
+resultado = palabras.repeat(3);
+document.write(resultado);
