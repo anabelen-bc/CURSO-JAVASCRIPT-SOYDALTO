642e0c3a51f5cf70f1c420cdf52aec1a0e282d94
anabelen-bc <anabelenn.bc@gmail.com>
2023-03-23 11:13:43 -0300

    Objetc Math - Propiedades

---
+++ Capitulo 4/ObjetoMath/Propiedades/codigo.js
@@ -0,0 +1,31 @@
+// PI - nos devuelve el numero PI
+let numero = Math.PI;
+document.write(numero + "<br>" + "<br>");
+// SQRT1_2 saca la raiz cuadrada de un medio 
+let numero1 = Math.SQRT1_2;
+document.write(numero1 + "<br>")
+
+// SQRT2 raiz cuadrada de dos
+let numero2 = Math.SQRT2;
+
+document.write(numero2 + "<br>");
+//CONSTANTES 
+// E constante de Euler, la base de los logaritmos
+let num = Math.E;
+document.write(num + "<br>");
+
+// LN2 logaritmo natural de 2
+let num1 = Math.LN2;
+document.write(num1 + "<br>");
+
+// LN10 logaritmo natural de 10
+let num2 = Math.LN10;
+document.write(num2 + "<br>");
+
+// LOG2E logaritmo E con base dos
+let num3 = Math.LOG2E;
+document.write(num3 + "<br>");
+
+// LOG10E logartimo E con base 10
+let num4 = Math.LOG10E;
+document.write(num4 + "<br>");
