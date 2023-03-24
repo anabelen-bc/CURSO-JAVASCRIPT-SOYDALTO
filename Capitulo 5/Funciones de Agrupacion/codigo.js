f2ee53d039082e8701bc5780a6877c199531d000
anabelen-bc <anabelenn.bc@gmail.com>
2023-03-23 22:10:20 -0300

    Console - Funciones de Agrupacion

---
+++ Capitulo 5/Funciones de Agrupacion/codigo.js
@@ -0,0 +1,11 @@
+console.group()
+console.log("Hola");
+console.log("Como");
+console.log("Estas?");
+console.group("Frutas")
+console.log("Banana");
+console.log("Mandarina");
+console.log("Pera");
+console.groupEnd();
+console.groupEnd();
+console.groupCollapsed("Frutas"); //Nos muestra devuelta el grupo, pero cerrado, tenemos que expandir para ver lo que contiene 
