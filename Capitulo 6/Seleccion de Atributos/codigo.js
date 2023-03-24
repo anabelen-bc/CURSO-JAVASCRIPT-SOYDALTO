fd4e82cccd47a0e59b35531d660b3022cb4f3ada
anabelen-bc <anabelenn.bc@gmail.com>
2023-03-24 11:18:23 -0300

    Seleccion de Atributos

---
+++ Capitulo 6/Seleccion de Atributos/codigo.js
@@ -0,0 +1,10 @@
+
+
+const rangoEtario = document.querySelector(".rangoEtario");
+// rangoEtario.setAttribute("type", "text");
+
+// valorDelAtributo= rangoEtario.getAttribute("type"); 
+
+// document.write(rangoEtario.getAttribute("type"));
+
+valorDelAtributo = rangoEtario.removeAttribute("type"); //Elimina el type
