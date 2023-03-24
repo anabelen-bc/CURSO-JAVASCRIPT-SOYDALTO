871d5e16685f0f86eb28892115234a9b9357370b
anabelen-bc <anabelenn.bc@gmail.com>
2023-03-24 09:10:59 -0300

    Actividad 5 - Problema B

--- Capitulo 5/Actividad 5/Problema B/codigo.js
+++ Capitulo 5/Actividad 5/Problema B/codigo.js
@@ -1,39 +1,28 @@
-const materias = {
-    fisica: [90, 6, 4, "fisica"],
-    matematica: [84, 8, 2, "matematica"],
-    logica: [92, 8, 4, "logica"],
-    quimica: [96, 8, 4, "quimica"],
-    calculo: [91, 6, 3, "calculo"],
-    programacion: [79, 7, 4, "programacion"],
-    biologia: [75, 9, 2, "biologia"],
-    bbdd: [98, 9, 1, "bbdd"],
-    algebra: [100, 10, 4, "algebra"],
-}
+let trabajo = "240 minutos de trabajo";
+let estudio = "100 minutos de estudio";
+let tp = "100 minutos de trabajos practicos";
+let homework = "30 minutos de cosas de la casa";
+let descanso = "10 mintunos de denscanso";
 
-const aprobo = () => {
-    for (materia in materias) {
-        let asistencias = materias[materia][0];
-        let promedio = materias[materia][1];
-        let trabajos = materias[materia][2];
+console.log("TAREA");
 
-        console.log(materias[materia][3]);
-        if (asistencias >= 90) {
-            console.log("%c    Aprobado", "color:green");
-        } else {
-
-            console.log("%c  Desprobado", "color:red");
-        }
-        if (promedio >= 7) {
-            console.log("%c Promedio en orden", "color:green");
-        } else {
-            console.log("%c Promedio desaprobado", "color:red");
-        }
-        if (trabajos >= 3) {
-            console.log("%c Trabajos Practicos en Orden", "color:green");
-        } else {
-            console.log("%c Faltan Trabajos Practios", "color:red");
-        }
+for (var i = 0; i < 14; i++) {
+    if (i == 0){
+        console.group("semana 1");
+    }
+console.groupCollapsed("dia " +(i+1));     
+console.log(trabajo);
+console.log(descanso);
+console.log(estudio);
+console.log(tp);
+console.log(homework);
+console.groupEnd();
+if (i == 7){
+    {
+        console.groupEnd();
+        console.groupCollapsed("Semana 2");
     }
 }
-
-aprobo();
+}
+console.groupEnd();
+console.groupEnd();
