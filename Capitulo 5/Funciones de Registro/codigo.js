02b2810edeca5851c3360f1053a655c7b498b8a4
anabelen-bc <anabelenn.bc@gmail.com>
2023-03-23 21:47:52 -0300

    Console - Funciones de Registro

---
+++ Capitulo 5/Funciones de Registro/codigo.js
@@ -0,0 +1,8 @@
+console.assert(5 < 4) // Nos muestra un mensaje de error en la consola si la afirmacion no se cumple
+console.clear() // Limpia la consola 
+console.error("Que hiciste?") // Nos muestra el mensaje de error 
+console.info("Argentina Campeon del Mundo")// Emite un mensaje informativo
+console.log()// Es un mensaje de depuracion SE USA MAS QUE EL INFO
+console.table([2, 2, ,2 ,2, 5, 5, 5]) // Toma un argumento como: data, que debe ser una array
+console.warn("Para que esto se ejecute hay que darle clic") // Imprime un mensaje de advertencia en la consola
+console.dir([2, 2, ,2 ,2, 5, 5, 5]) // Despliega una lista interactiva de las propiedades del objeto especificado
