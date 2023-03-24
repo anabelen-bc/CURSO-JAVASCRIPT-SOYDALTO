4fef2218f680f69eabc62e41b9756fdbd996cb4a
anabelen-bc <anabelenn.bc@gmail.com>
2023-03-24 11:03:11 -0300

    Concepto DOM - Seleccion de Elementos

---
+++ Capitulo 6/DOM/codigo.js
@@ -0,0 +1,8 @@
+document.getElementById("texto");
+document.write(texto);
+document.getElementsByTagName("p");
+document.write(parrafo[0]);
+document.querySelector(".parrafo");
+document.write(parrafo)
+document.querySelectorAll(".parrafo");
+document.write(parrafo[5]);
