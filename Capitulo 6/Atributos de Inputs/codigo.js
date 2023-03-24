1e2c9757cdaef533443b7cc7c325c5453201f126
anabelen-bc <anabelenn.bc@gmail.com>
2023-03-24 13:06:55 -0300

    Atributos del Input

--- Capitulo 6/Atributos de Inputs/codigo.js
+++ Capitulo 6/Atributos de Inputs/codigo.js
@@ -1,1 +1,25 @@
 const input = document.querySelector(".input-normal");
+
+// document.write(input.className + "<br>");
+
+// document.write(input.value + "<br>");
+
+input.type = "number"; 
+
+const input1 = document.querySelector(".input-normal1");
+
+input1.type ="color";
+
+const input2 = document.querySelector(".input-normal2");
+
+input2.type ="range";
+
+const input3 = document.querySelector(".input-normal3");
+
+input3.accept = "image/png";
+
+const input4 = document.querySelector(".input-normal4");
+
+input4.setAttribute("minlenght","4");
+input4.placeholder= "Como es tu nombre"
+input4.required = "true";
