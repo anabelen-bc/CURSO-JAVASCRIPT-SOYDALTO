346c50108a2b608b89793e59eea48a566645d889
anabelen-bc <anabelenn.bc@gmail.com>
2023-03-24 11:43:36 -0300

    Atributos Globales

---
+++ Capitulo 6/Atributos Globales/codigo.js
@@ -0,0 +1,9 @@
+const titulo = document.querySelector(".titulo");
+titulo.setAttribute("contenteditable", "true");
+// titulo.setAttribute("dir", "rtl");
+titulo.setAttribute("hidden", "true"); // el elemento se oculta
+
+
+titulo.setAttribute("tabindex", "0");
+
+titulo.setAttribute("title", "jajaja");
