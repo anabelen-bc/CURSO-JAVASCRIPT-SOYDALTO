9167b09ad86093acf82eeaffb6a99a8542dbd756
anabelen-bc <anabelenn.bc@gmail.com>
2023-03-26 19:07:31 -0300

    Obsoletos

---
+++ Capitulo 11/codigo.js
@@ -0,0 +1,40 @@
+let variable = "nombre";
+
+let resultado = variable.big();
+document.write(resultado + "<br>");
+
+let resultado1 = variable.blink();
+document.write(resultado1 + "<br>");
+
+let resultado2 = variable.bold();
+document.write(resultado2 + "<br>");
+
+let resultado3 = variable.fixed();
+document.write(resultado3 + "<br>");
+
+let resultado4 = variable.fontcolor("blue");
+document.write(resultado4 + "<br>");
+
+let resultado5 = variable.fontsize(24);
+document.write(resultado5 + "<br>");
+
+let resultado6 = variable.italics();
+document.write(resultado6 + "<br>");
+
+let resultado7 = variable.anchor();
+document.write(resultado7 + "<br>");
+
+let resultado8 = variable.link("https://youtube.com");
+document.write(resultado8 + "<br>");
+
+let resultado9 = variable.small();
+document.write(resultado9 + "<br>");
+
+let resultado10 = variable.strike();
+document.write(resultado10 + "<br>");
+
+let resultado11 = variable.sub();
+document.write(resultado11 + "<br>");
+
+let resultado12 = variable.sup();
+document.write(resultado12 + "<br>");
