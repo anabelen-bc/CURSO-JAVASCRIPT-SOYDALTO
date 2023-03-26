58b256aa2194ac5e9f84a1403b70f64d48b40257
anabelen-bc <anabelenn.bc@gmail.com>
2023-03-25 23:31:05 -0300

    Pestañas Elements

---
+++ Capitulo 8/codigo.js
@@ -0,0 +1,5 @@
+// Desde la pestaña element, podemos hacer modificaciones para poder visualizarla y luego vamos a poder  obtenerlas mediante el copiado ctrl + c
+// El codigo css se actuliza en tiempo real, mientras que el de js no.
+//
+//
+//
