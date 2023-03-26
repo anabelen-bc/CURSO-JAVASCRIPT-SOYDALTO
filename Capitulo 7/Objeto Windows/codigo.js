acdd21a6b84865fb9d3c91bd280540a561046384
anabelen-bc <anabelenn.bc@gmail.com>
2023-03-25 21:32:56 -0300

    Propiedades de window

---
+++ Capitulo 7/Objeto Windows/codigo.js
@@ -0,0 +1,8 @@
+let ventana = window.open("https:youtube.com") //Nos abre una ventana de google con la pagina que se encuentra entre parentesis
+ventana.close(); // Nos cierra la ventana que abrimos ateriormente 
+// .closed, nos sirve para preguntar desde consola si la ventana esta cerrada o abierta.. nos devuelve los valores de true o false
+// si en consola ponemos del navegador indicamos WINDOW.STOP() frena la carga del sitio, es como si apretaramos la (x)
+window.alert("Hola Mundo") // Nos crea la ventana emergente con el mensaje que se encuentra dentro del parentesis cuando abrimos el navegador
+print() // Nos permite escribir a partir de js
+prompt("Sirve para pedirle datos al usuario"); // Y nos devuelve string
+confirm("Estas seguro que queres salir del sitio web?") // Hace la pregunta que se encuentra entre parentesis, y nos devuelve como valor true o false
