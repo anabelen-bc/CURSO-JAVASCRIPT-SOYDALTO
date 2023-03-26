e22c4e33271ce75397e4a1a7a8a60c15c974b9ea
anabelen-bc <anabelenn.bc@gmail.com>
2023-03-26 09:25:53 -0300

    ELEMENTS - RESOURCE - NETWORK - TIMELINE/PERFORMANCE

--- Capitulo 8/codigo.js
+++ Capitulo 8/codigo.js
@@ -1,5 +1,7 @@
 // Desde la pestaña element, podemos hacer modificaciones para poder visualizarla y luego vamos a poder  obtenerlas mediante el copiado ctrl + c
-// El codigo css se actuliza en tiempo real, mientras que el de js no.
+// El codigo css se actuliza en tiempo real(con un plugin de tiempo real), mientras que el de js no.
 //
 //
-//
+// //p:hover{
+//     color:red; 
+// } Lo que se puede realizar tambien es que cuando a un elemento le damos hover, no hace falta que el mouse este por arriba de el, desde las herramientas del desarrollador se puede forzar ese y otros efectos mas
