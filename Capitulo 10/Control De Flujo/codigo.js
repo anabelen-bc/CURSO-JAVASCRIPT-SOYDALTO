907dc934854ef437483f623fe21e0c3be311e5a8
anabelen-bc <anabelenn.bc@gmail.com>
2023-03-26 16:50:17 -0300

    Control de flujo

---
+++ Capitulo 10/Control De Flujo/codigo.js
@@ -0,0 +1,12 @@
+let nombre = "Ani"; // Esta es una variable que existe en todo el programa
+
+alert(nombre); //El flujo del programa, es como se ejecutan las lineas que nostros escrimos, en este ejemplo primero se crea la variable y despues nos devuelve el cartel.
+
+
+{
+    let nombre = "Pedro" //Esta es una variable local
+} // Esto es una sentencia de bloque
+
+if (3 > 2){
+    alert("Es cierto")
+} // Sentencia de control de flujo
