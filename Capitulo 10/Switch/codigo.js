ed4920ddc8383609f22627fb58c64f49f148959d
anabelen-bc <anabelenn.bc@gmail.com>
2023-03-26 17:01:07 -0300

    Switch

---
+++ Capitulo 10/Switch/codigo.js
@@ -0,0 +1,16 @@
+let expresion = "Banana";
+
+
+switch (expresion) {
+    case "Banana":
+        console.log("Esta fruta es amarrilla");
+        break;
+    case "Pera":
+        console.log("Tiene forma de mi tio roberto");
+        break;
+    case "Manzana":
+        console.log("Los primeros humanos pecaron comiendo");
+        break;
+        default: alert("no es niguna")
+
+}
