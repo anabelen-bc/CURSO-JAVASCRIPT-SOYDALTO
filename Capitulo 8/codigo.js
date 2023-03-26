450a4322c8517c933358dea36e993d9f476fea1e
anabelen-bc <anabelenn.bc@gmail.com>
2023-03-26 09:27:04 -0300

    Elements - Resource - Network - Timeline/Performance

--- Capitulo 8/codigo.js
+++ Capitulo 8/codigo.js
@@ -1,3 +1,4 @@
+//PESTAÑA ELEMENTS
 // Desde la pestaña element, podemos hacer modificaciones para poder visualizarla y luego vamos a poder  obtenerlas mediante el copiado ctrl + c
 // El codigo css se actuliza en tiempo real(con un plugin de tiempo real), mientras que el de js no.
 //
@@ -5,3 +6,18 @@
 // //p:hover{
 //     color:red; 
 // } Lo que se puede realizar tambien es que cuando a un elemento le damos hover, no hace falta que el mouse este por arriba de el, desde las herramientas del desarrollador se puede forzar ese y otros efectos mas
+//PESTAÑA RESOURCE 
+//Podemos visualizar, y modificar el codigo de las pestañas desde chrome
+//Lo podemos guardar en tiempo real, y se crea en nuestro js local
+//New snippet: crea fragmentos de codigo, el cual se guarda en sources y nos sirve para usarlo las veces que queramos
+//PESTAÑA NETWORK
+//Sirven mas para analizar el codigo, para que nos muestre informacion tenemos que cargar la pagina con las herramientas abierta. 
+//Nos cuenta que inicializa cada archivo, y cuanto pesa 
+//te muestra cuantos archivos fueron solicitadas
+//Cuanto tardo en cargar el DOM - y cuanto tardo en cargar toda la pagina completa
+//Existen recursos que se almacenan en la memoria cache para que cuando volvamos a cargar la pagina no tarde tanto
+//PESTAÑA TIMELINE/PERFORMANCE 
+//Nos muestra la informacion de los recursos que consume la pagina que se esta utilizando
+//Se puede analizar por fragmentes tambien 
+//Se puede grabar todos los recursos que consume la pagina segundo a segundo
+//
